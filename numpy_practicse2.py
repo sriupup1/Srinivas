9d5c055677396c0ef20beaebbbf591da384e16bc
sriupup1 <raj.srinivas008@gmail.com>
2019-01-24 15:41:18 +0530

    second numpy file

---
+++ numpy_practicse2.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 19 18:30:16 2019
+
+@author: Ch Srihari Varma
+"""
+import numpy as np
+a=np.arange(1,10,2)
+print(a)
+print(a[1])
+print(a[1:])
+b=np.linspace(0,1,5)
+print(b)
+print(np.eye(4))
+print(np.zeros([4,4]))
